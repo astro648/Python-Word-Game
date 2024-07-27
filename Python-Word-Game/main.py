0c0edbf875f56aca4f247b98112c0dfd372b54b3
Ayaan <53174223+astro648@users.noreply.github.com>
2024-07-27 21:42:20 +0800

    idea

---
+++ Python-Word-Game/main.py
@@ -0,0 +1,9 @@
+'''
+- CSV of English words is used
+- Normal difficulty: words from 5-15 characters and 1-3 blank spaces
+- Hard difficulty: 10+ character words and 5 blank spaces
+- Single-Player: one person plays
+- Pass & Play: 2 players can play and compete to win
+- Each round won is +1 score
+- 3 health at start of each game, each round lost is -1 health
+'''
